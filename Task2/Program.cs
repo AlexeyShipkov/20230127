c24b285b84cb5cc33eb5444ebe997320fce62d12
AlexeyShipkov <alexeyshipkov@gmail.com>
2023-01-26 14:42:26 +0300

    Task2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("input to numbers");
+Console.Write("the first is: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("and the second is: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+Console.WriteLine($"max = {a}, min = {b}");
+else
+Console.WriteLine($"max = {b}, min = {a}");
