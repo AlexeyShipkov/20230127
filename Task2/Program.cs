ef90481feae9c4950eac9808282b2cc6f3160295
AlexeyShipkov <alexeyshipkov@gmail.com>
2023-01-26 15:04:17 +0300

    Task2 done, added comments

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,9 +1,19 @@
-﻿Console.WriteLine("input to numbers");
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+*/
+
+// ввод данных
+Console.WriteLine("input to numbers");
 Console.Write("the first is: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("and the second is: ");
 int b = Convert.ToInt32(Console.ReadLine());
-if (a>b)
+// сравнение и вывод
+
+if (a>b) 
 Console.WriteLine($"max = {a}, min = {b}");
+else
+if (a==b)
+Console.WriteLine($"max = min, {a} = {b}");
 else
 Console.WriteLine($"max = {b}, min = {a}");
